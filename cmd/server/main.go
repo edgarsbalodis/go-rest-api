e3a3ee4a0644b781e0d4da8bcf97bdfdfac2022f
Edgars Balodis <baloddis@gmail.com>
2023-07-20 23:21:12 +0200

    db migration, query test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/edgarsbalodis/go-rest-api/internal/comment"
+	"github.com/edgarsbalodis/go-rest-api/internal/db"
+)
 
 // Run - is going to be responsible for
 // the instantiation and startup of our
 // go application
 func Run() error {
 	fmt.Println("starting up out application")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to the database")
+		return err
+	}
+
+	if err := db.MigrateDB(); err != nil {
+		fmt.Println("failed to migrate database")
+		return nil
+	}
+
+	cmtService := comment.NewService(db)
+	fmt.Println(cmtService.GetComment(context.Background(), "2c9dcdf8-273f-11ee-be56-0242ac120002"))
 	return nil
 }
 
